c0b0487a2f6dbca8f60517916b32e362d397fd26
Glebtov <glebtov@gmail.com>
2023-09-05 17:15:34 +0300

    was updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import TestItem from "./TestItem/TestItem"
 
 function App() {
-    return ( < div >
-        <
-        h1 > Let 's start</h1> < /div >
-        TestItem();
-    );
+    return ( 
+    <div>
+        <h1 > Let 's start</h1>
+        <h2></h2>
+    </div>    
+);
 }
 
 export default App;
--- src/TestItem/TestItem.js
+++ src/TestItem/TestItem.js
@@ -1,5 +1,5 @@
 function TestItem() {
-    return <div > < h1 > Hi! < /h1></div >
+    return <div > <h1> Hi! </h1></div >
 }
 
 export default TestItem;
