4ac5e8173dca32372783c7eb8f7660a8d3813030
Hlieb <glebtov@gmail.com>
2023-09-09 21:42:36 +0300

    was updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-function App() {
-    return (
-        <h1>Hello</h1>
-    );
-=======
-import HeaderItem from "./head/Head"
+import HeaderItem from "./components/Header"
 
 function App() {
     return ( 
        HeaderItem()
-);
->>>>>>> e44680131e7faa2e0ce9fc28f8eb8ce6d5ba8ac6
-}
+);}
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+function HeaderItem() {
+    return ( 
+        <header>
+            <div>
+                <img src="./image 3.png" className="logo_header"></img>
+                <img src="./img/material-symbols_call-outline-sharp.png" className="phone_header"></img>
+                <p className="phonenumber"></p>
+                <img src="./img/Frage 6.png" className="lines_header"></img>
+            </div>
+            <div class="high_text_image">
+                <h1 class="high_text">Монтаж натяжних стель</h1>
+                <h4 class="high_under_text">Розробка дизайну та освітлення стельового простору</h4>
+            </div>
+        </header>
+    );
+}
+
+export default HeaderItem
