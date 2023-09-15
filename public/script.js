a5a33338c5dd6662595043f88497506f76a7dca2
Glebtov <glebtov@gmail.com>
2023-09-15 23:02:52 +0300

    was updated

--- public/script.js
+++ public/script.js
@@ -1,3 +1,10 @@
 function showPhoto() {
-    document.querySelector(".list_first").style.display = "block";
+    var element = document.querySelector('list_first');
+
+    if (element.style.display === 'none') {
+        element.style.display = "block";
+    }
+    else {
+        element.style.display = "none";
+    }
 };
