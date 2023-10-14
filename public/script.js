8bb85db7658a40e5725a70788e21f340d231ad17
glebtov <glebtov@gmail.com>
2023-10-14 18:14:32 +0300

    was updated

--- public/script.js
+++ public/script.js
@@ -1,9 +1,95 @@
-function showPhoto() {
-    var element = document.getElementById('listFirst');
+function showPhotoFirst() {
+    const element = document.getElementById('listFirst');
+    const button = document.getElementById('listFirstImage');
 
-    if (element.style.display === 'none') {
-        element.style.display = "flex";
-    } else {
+    if (element.style.display === 'flex') {
         element.style.display = "none";
+        button.style.transform = "rotate(-360deg)"
+    } else {
+        element.style.display = "flex";
+        button.style.transform = "rotate(180deg)"
+    }
+
+
+};
+
+function showPhotoSecond() {
+    const element = document.getElementById('listSecond');
+    const button = document.getElementById('listSecondImage');
+
+
+    if (element.style.display === 'flex') {
+        element.style.display = "none";
+        button.style.transform = "rotate(-360deg)"
+    } else {
+        element.style.display = "flex";
+        button.style.transform = "rotate(180deg)"
+    }
+};
+
+function showPhotoThird() {
+    const element = document.getElementById('listThird');
+    const button = document.getElementById('listThirdImage');
+
+
+    if (element.style.display === 'flex') {
+        element.style.display = "none";
+        button.style.transform = "rotate(-360deg)"
+    } else {
+        element.style.display = "flex";
+        button.style.transform = "rotate(180deg)"
+    }
+};
+
+function showPhotoFourth() {
+    const element = document.getElementById('listFourth');
+    const button = document.getElementById('listFourthImage');
+
+
+    if (element.style.display === 'flex') {
+        element.style.display = "none";
+        button.style.transform = "rotate(-360deg)"
+    } else {
+        element.style.display = "flex";
+        button.style.transform = "rotate(180deg)"
+    }
+};
+function showPhotoFifth() {
+    const element = document.getElementById('listFifth');
+    const button = document.getElementById('listFifthImage');
+
+
+    if (element.style.display === 'flex') {
+        element.style.display = "none";
+        button.style.transform = "rotate(-360deg)"
+    } else {
+        element.style.display = "flex";
+        button.style.transform = "rotate(180deg)"
+    }
+};
+function showPhotoSixth() {
+    const element = document.getElementById('listSixth');
+    const button = document.getElementById('listSixthImage');
+
+
+    if (element.style.display === 'flex') {
+        element.style.display = "none";
+        button.style.transform = "rotate(-360deg)"
+    } else {
+        element.style.display = "flex";
+        button.style.transform = "rotate(180deg)"
+    }
+};
+function showPhotoSeventh() {
+    const element = document.getElementById('listSeventh');
+    const button = document.getElementById('listSeventhImage');
+
+
+    if (element.style.display === 'flex') {
+        element.style.display = "none";
+        button.style.transform = "rotate(-360deg)"
+    } else {
+        element.style.display = "flex";
+        button.style.transform = "rotate(180deg)"
     }
 };
