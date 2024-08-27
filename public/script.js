61d5251570172e6bd831ca9bc4cf8393ab5226f2
Glebtov <glebtov@gmail.com>
2024-08-27 19:29:42 +0300

    was updated

--- public/script.js
+++ public/script.js
@@ -1,95 +1,108 @@
 function showPhotoFirst() {
-    const element = document.getElementById('listFirst');
-    const button = document.getElementById('listFirstImage');
+  const element = document.getElementById("listFirst");
+  const button = document.getElementById("listFirstImage");
+  const text = document.getElementById("listFirstText");
 
-    if (element.style.display === 'flex') {
-        element.style.display = "none";
-        button.style.transform = "rotate(-360deg)"
-    } else {
-        element.style.display = "flex";
-        button.style.transform = "rotate(180deg)"
-    }
-
-
-};
+  if (element.style.display === "flex") {
+    element.style.display = "none";
+    button.style.transform = "rotate(-360deg)";
+    text.style.textDecoration = "none";
+  } else {
+    element.style.display = "flex";
+    button.style.transform = "rotate(180deg)";
+    text.style.textDecoration = "underline";
+  }
+}
 
 function showPhotoSecond() {
-    const element = document.getElementById('listSecond');
-    const button = document.getElementById('listSecondImage');
+  const element = document.getElementById("listSecond");
+  const button = document.getElementById("listSecondImage");
+  const text = document.getElementById("listSecondText");
 
-
-    if (element.style.display === 'flex') {
-        element.style.display = "none";
-        button.style.transform = "rotate(-360deg)"
-    } else {
-        element.style.display = "flex";
-        button.style.transform = "rotate(180deg)"
-    }
-};
+  if (element.style.display === "flex") {
+    element.style.display = "none";
+    button.style.transform = "rotate(-360deg)";
+    text.style.textDecoration = "none";
+  } else {
+    element.style.display = "flex";
+    button.style.transform = "rotate(180deg)";
+    text.style.textDecoration = "underline";
+  }
+}
 
 function showPhotoThird() {
-    const element = document.getElementById('listThird');
-    const button = document.getElementById('listThirdImage');
+  const element = document.getElementById("listThird");
+  const button = document.getElementById("listThirdImage");
+  const text = document.getElementById("listThirdText");
 
-
-    if (element.style.display === 'flex') {
-        element.style.display = "none";
-        button.style.transform = "rotate(-360deg)"
-    } else {
-        element.style.display = "flex";
-        button.style.transform = "rotate(180deg)"
-    }
-};
+  if (element.style.display === "flex") {
+    element.style.display = "none";
+    button.style.transform = "rotate(-360deg)";
+    text.style.textDecoration = "none";
+  } else {
+    element.style.display = "flex";
+    button.style.transform = "rotate(180deg)";
+    text.style.textDecoration = "underline";
+  }
+}
 
 function showPhotoFourth() {
-    const element = document.getElementById('listFourth');
-    const button = document.getElementById('listFourthImage');
+  const element = document.getElementById("listFourth");
+  const button = document.getElementById("listFourthImage");
+  const text = document.getElementById("listFourthText");
 
-
-    if (element.style.display === 'flex') {
-        element.style.display = "none";
-        button.style.transform = "rotate(-360deg)"
-    } else {
-        element.style.display = "flex";
-        button.style.transform = "rotate(180deg)"
-    }
-};
+  if (element.style.display === "flex") {
+    element.style.display = "none";
+    button.style.transform = "rotate(-360deg)";
+    text.style.textDecoration = "none";
+  } else {
+    element.style.display = "flex";
+    button.style.transform = "rotate(180deg)";
+    text.style.textDecoration = "underline";
+  }
+}
 function showPhotoFifth() {
-    const element = document.getElementById('listFifth');
-    const button = document.getElementById('listFifthImage');
+  const element = document.getElementById("listFifth");
+  const button = document.getElementById("listFifthImage");
+  const text = document.getElementById("listFifthText");
 
-
-    if (element.style.display === 'flex') {
-        element.style.display = "none";
-        button.style.transform = "rotate(-360deg)"
-    } else {
-        element.style.display = "flex";
-        button.style.transform = "rotate(180deg)"
-    }
-};
+  if (element.style.display === "flex") {
+    element.style.display = "none";
+    button.style.transform = "rotate(-360deg)";
+    text.style.textDecoration = "none";
+  } else {
+    element.style.display = "flex";
+    button.style.transform = "rotate(180deg)";
+    text.style.textDecoration = "underline";
+  }
+}
 function showPhotoSixth() {
-    const element = document.getElementById('listSixth');
-    const button = document.getElementById('listSixthImage');
+  const element = document.getElementById("listSixth");
+  const button = document.getElementById("listSixthImage");
+  const text = document.getElementById("listSixthText");
 
-
-    if (element.style.display === 'flex') {
-        element.style.display = "none";
-        button.style.transform = "rotate(-360deg)"
-    } else {
-        element.style.display = "flex";
-        button.style.transform = "rotate(180deg)"
-    }
-};
+  if (element.style.display === "flex") {
+    element.style.display = "none";
+    button.style.transform = "rotate(-360deg)";
+    text.style.textDecoration = "none";
+  } else {
+    element.style.display = "flex";
+    button.style.transform = "rotate(180deg)";
+    text.style.textDecoration = "underline";
+  }
+}
 function showPhotoSeventh() {
-    const element = document.getElementById('listSeventh');
-    const button = document.getElementById('listSeventhImage');
+  const element = document.getElementById("listSeventh");
+  const button = document.getElementById("listSeventhImage");
+  const text = document.getElementById("listSeventhText");
 
-
-    if (element.style.display === 'flex') {
-        element.style.display = "none";
-        button.style.transform = "rotate(-360deg)"
-    } else {
-        element.style.display = "flex";
-        button.style.transform = "rotate(180deg)"
-    }
-};
+  if (element.style.display === "flex") {
+    element.style.display = "none";
+    button.style.transform = "rotate(-360deg)";
+    text.style.textDecoration = "none";
+  } else {
+    element.style.display = "flex";
+    button.style.transform = "rotate(180deg)";
+    text.style.textDecoration = "underline";
+  }
+}
