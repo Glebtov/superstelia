83342b0eead0197be07b50b89fc844a6d360e2a3
glebtov <glebtov.com>
2024-08-28 21:50:24 +0300

    was updated

--- public/menu.js
+++ public/menu.js
@@ -1,10 +1,20 @@
 const menu = document.getElementById("menu");
-const body = document.getElementsByTagName("body");
+const modal = document.getElementById("modal");
 
 function showMenu() {
   menu.style.right = "0%";
+  document.body.style.overflow = "hidden";
+  modal.style.display = "block";
 }
 
 function closeMenu() {
   menu.style.right = "-100%";
+  document.body.style.overflow = "auto";
+  modal.style.display = "none";
 }
+
+modal.addEventListener("click", (event) => {
+  if (event.target == modal) {
+    closeMenu();
+  }
+});
